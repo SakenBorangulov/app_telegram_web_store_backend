d4531aa827658c2b82dfd0b0a364290579fb4d51
Saken Borangulov <borangulovsaken@gmail.com>
2025-08-09 17:15:02 +0300

    host address added

--- index.js
+++ index.js
@@ -74,4 +74,4 @@ app.post("/web-data", async ( req, res) => {
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+app.listen(PORT,"0.0.0.0",  () => console.log(`Server started on PORT ${PORT}`))
